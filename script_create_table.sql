32dfc1dd5cf81fc2bbe3c13dffa0f2ad86c5dade
Yuliya Maslova <uu.naumova@gmail.com>
2023-10-06 15:32:45 +0300

    исправление

--- script_create_table.sql
+++ script_create_table.sql
@@ -1,9 +1,8 @@
 create table PERSONS(
     name varchar(255),
     surname varchar(255),
+    phone_number varchar(255),
+    city_of_living varchar(255),
     age int,
-    primary key (name, age)
-)
-
-alter table PERSONS add column phone_number varchar(255);
-alter table PERSONS add column city_of_living varchar(255);
+    primary key (name, surname, age)
+);
